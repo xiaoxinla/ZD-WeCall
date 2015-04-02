93bdb4827f57135267dcd7d2659039cebd3a9d92
Kim <likunmin5@gmail.com>
2015-04-02 14:42:21 +0800

    Modified database & databasehelper

--- Contacts/src/com/wecall/contacts/entity/Database.java
+++ Contacts/src/com/wecall/contacts/entity/Database.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -18,85 +20,177 @@ import android.util.Log;
 public class Database {
 	private SQLiteDatabase db = null;
 	private DatabaseHelper helper;
-	private final static String TB_NAME = "Contacts";
+	private final static String MAIN_TABLE = "main";
+	private final static String ADDRESS_TABLE = "address";
+	private final static String TAG_TABLE = "tag";
+	private final static String PHONE_TABLE = "phoneNumber";
+	// 为联系人分配的唯一标识
+	private static int id;
+	// id保存到preference中
+	private static SharedPreferences preferences;
 	
 	public Database(Context context)
 	{
 		helper = new DatabaseHelper(context, "Contacts.db");
+		// 建立到数据库的可写连接
+		db = helper.getWritableDatabase();
+		// 通过shared preference初始化id值
+		preferences = context.getSharedPreferences("database", Context.MODE_PRIVATE);
+		id = preferences.getInt("id_count", 1);
 	}
 	
-	public void createTable(String tableName)
-	{
-		db = helper.getWritableDatabase();
-		String sql = "create table if not exists "
-				+ tableName
-				+ "(_id integer primary key, "
-				+ "number varchar(20) "
-				+ ");";
-		try
-		{
-			db.execSQL(sql);	
-		} catch (SQLException se)
-		{
-			Log.i("err", "create table failed");
+	/*
+	 * 向数据库插入新的一个联系人
+	 * 如果插入成功会同时设置item对象的id.
+	 */
+	public void insert(ContactItem item)
+	{		
+		ContentValues values = new ContentValues();
+		db.beginTransaction();
+		try {
+			// 插入main表
+			values.put("c_id", id);
+			values.put("name", item.getName());
+			values.put("fullPinyin", item.getFullPinyin());
+			values.put("simplePinyin", item.getSimplePinyin());
+			values.put("sortLetter", item.getSortLetter());
+			values.put("note", item.getNote());		
+			db.insert(MAIN_TABLE, null, values);
+			
+			// 插入phoneNum表
+			// TODO: 多个Number
+			values.clear();
+			values.put("c_id", id);
+			values.put("phoneNumber", item.getPhoneNumber());
+			db.insert(PHONE_TABLE, null, values);
+			
+			// 插入address表
+			// TODO: 可能多个address
+			values.clear();
+			values.put("c_id", id);
+			values.put("address", item.getAddress());
+			db.insert(ADDRESS_TABLE, null, values);
+			
+			// 插入tag表
+			ArrayList<String> list = item.getLabels();
+			if (list != null) {
+				for (String l : list) {
+					values.clear();
+					values.put("c_id", id);
+					values.put("tag", l);
+					db.insert(TAG_TABLE, null, values);
+				}
+			}
+			db.setTransactionSuccessful();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Log.i("err", "data insert failed.");
+		} finally {
+			item.setId(id);
+			id++;
+			db.endTransaction();
+		}
+		Editor editor = preferences.edit();
+		editor.putInt("id_count", id);
+		editor.commit();
+	}
+	
+	/*
+	 * 通过联系人标识号删除一条联系人记录
+	 * @parameter id， 联系人标识号
+	 */
+	public void delete(int id)
+	{			
+		try {
+			// 打开外键约束，确保级联删除，据说Android2.2后才支持
+			db.execSQL("PRAGMA foreign_keys=ON");
+			db.delete(MAIN_TABLE, "c_id = ?", new String[]{ Integer.toString(id) } );
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Log.i("err", "delete based on id failed.");
 		}
 	}
 	
-	public void insert(ContactItem item)
-	{
-		// 建立到数据库的可写连接
-		db = helper.getWritableDatabase();
-		
-		ContentValues values = new ContentValues();
-		values.put("name", item.getName());
-		values.put("phoneNumber", item.getPhoneNumber());
-		values.put("address", item.getAddress());
-		values.put("fullPinyin", item.getFullPinyin());
-		values.put("simplePinyin", item.getSimplePinyin());
-		
-		db.insert(TB_NAME, null, values);
-	}
-	
-	public void delete(int id)
-	{
-		db = helper.getWritableDatabase();
-		
-		db.delete(TB_NAME, "_id = ?", new String[]{ Integer.toString(id) } );
-	}
-	
+	/*
+	 * 通过联系人标识号更新记录
+	 * 通过传入的item.id识别需要删除的行
+	 * FIXME: 目前只能全部列一起更新，可能效率低，需要重载更多参数的版本，指定条目更新
+	 * FIXME: 电话可能也需要加入id，否则不知道旧号码是否要删除
+	 */
 	public void update(ContactItem item)
-	{
-		db = helper.getWritableDatabase();
-		
+	{		
 		ContentValues values = new ContentValues();
-		values.put("name", item.getName());
-		values.put("phoneNumber", item.getPhoneNumber());
-		values.put("address", item.getAddress());
-		values.put("fullPinyin", item.getFullPinyin());
-		values.put("simplePinyin", item.getSimplePinyin());
-		
-		db.update(TB_NAME, values, "name = ?", new String[]{item.getName()});
+		db.beginTransaction();
+		try {
+			// 更新main表
+			values.put("name", item.getName());
+			values.put("fullPinyin", item.getFullPinyin());
+			values.put("simplePinyin", item.getSimplePinyin());
+			values.put("sortLetter", item.getSortLetter());
+			values.put("note", item.getNote());	
+			db.update(MAIN_TABLE, values, "c_id = ?", 
+					new String[]{Integer.toString(item.getId())});
+			
+			// 更新phoneNum表，就是插入新行
+			values.clear();
+			values.put("c_id", item.getId());
+			values.put("phoneNumber", item.getPhoneNumber());
+			db.insert(PHONE_TABLE, null, values);
+			
+			// 更新address表
+			values.clear();
+			values.put("c_id", item.getId());
+			values.put("address", item.getAddress());
+			db.update(ADDRESS_TABLE, values, "c_id = ?", 
+					new String[]{Integer.toString(item.getId())});
+			
+			// 插入tag表
+			ArrayList<String> list = item.getLabels();
+			if (list != null) {
+				for (String l : list) {
+					values.clear();
+					values.put("c_id", item.getId());
+					values.put("tag", l);
+					db.insert(TAG_TABLE, null, values);
+				}
+			}
+			db.setTransactionSuccessful();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Log.i("err", "data update failed.");
+		} finally
+		{
+			db.endTransaction();
+		}
 	}
 	
+	/*
+	 * 根据姓名来搜索
+	 * @TODO：提供更多搜索接口
+	 */
 	public ArrayList<ContactItem> query(String name)
 	{
 		ArrayList<ContactItem> list = null;
 		try {
-			db = helper.getReadableDatabase();
-			
-			Cursor cursor = db.query(TB_NAME, new String[] {"_id,name"}, "name=?", 
+			String table = MAIN_TABLE + " left outer join " + PHONE_TABLE + 
+					" on " + MAIN_TABLE + ".c_id = " + PHONE_TABLE + ".c_id" +
+					" left outer join " + ADDRESS_TABLE + 
+					" on " + MAIN_TABLE + ".c_id = " + ADDRESS_TABLE + ".c_id" +
+					" left outer join " + TAG_TABLE +
+					" on " + MAIN_TABLE + ".c_id = " + TAG_TABLE + ".c_id";
+			Cursor cursor = db.query(table, new String[] {"*"}, "name=?", 
 					new String[] {name}, null, null, null);
 			Log.i("Cursor", Integer.toString(cursor.getCount()));
 			list = convertCursorToItem(cursor);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Log.i("err", "query by name failed.");
 		}		
 		return list;
 	}
 	
 	/*
-	 * 提供直接执行sql语句的接口，调试用
+	 * 提供直接执行sql语句的接口，仅供调试用
 	 * @Deprecated
 	 */	
 	public void execSQL(String sql)
@@ -105,12 +199,18 @@ public class Database {
 		db.execSQL(sql);
 	}
 	
+	/*
+	 * 释放资源，应在Activity onDestory时调用
+	 */
 	public void close()
 	{
 		db.close();
 		helper.close();
 	}
 	
+	/*
+	 * 将查询结果cursor转化为列表，方便输出
+	 */	
 	private ArrayList<ContactItem> convertCursorToItem(Cursor cursor)
 	{
 		ArrayList<ContactItem> list = new ArrayList<ContactItem>();
@@ -123,9 +223,7 @@ public class Database {
 			
 			list.add(it);
 		}
-		
-		Log.i("size", Integer.toString(list.size()) );
-		
+				
 		return list;
 	}
 }
--- Contacts/src/com/wecall/contacts/entity/DatabaseHelper.java
+++ Contacts/src/com/wecall/contacts/entity/DatabaseHelper.java
@@ -1,12 +1,15 @@
 package com.wecall.contacts.entity;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.util.Log;
 
 /*
  * 辅助访问数据库
+ * 数据库包含4个表：main, phoneNumber, address, tag 
  * 
  * @author KM
  * 
@@ -15,15 +18,55 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 public class DatabaseHelper extends SQLiteOpenHelper{
 	// 数据库版本
     private static final int VERSION = 1;
-    // 创建表
-    private final String CREATE_TABLE = "create table if not exists " 
-    								+ "Contacts(_id integer primary key autoincrement, " 
-    								+ "name varchar(50), " 
-    								+ "phoneNumber varchar(20), "
-    								+ "address varchar(100), "
-    								+ "fullPinyin varchar(50), "
-    								+ "simplePinyin varchar(10) "
-    								+ ");";
+    
+    /* Main表，存基本信息
+     * 列依次为：c_id, name, fullPinyin, simplePinyin, sortLetter, note
+     */    
+    private final static 
+    String MAIN_TABLE = "CREATE TABLE IF NOT EXISTS main( "
+    					+ "c_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    					+ "name VARCHAR(100), "
+    					+ "fullPinyin VARCHAR(100), "
+    					+ "simplePinyin VARCHAR(20), "
+    					+ "sortLetter VARCHAR(5), "
+    					+ "note VARCHAR(255)"
+    					+ ");";
+    
+    /*
+     * PhoneNumber表，存多个电话
+     * 列依次为：c_id, phoneNumber
+     */
+    private final static 
+    String PHONE_NUMBER_TABLE = "CREATE TABLE IF NOT EXISTS phoneNumber(" 
+    					+ "c_id INTEGER NOT NULL, "
+    					+ "phoneNumber VARCHAR(50), "
+    					+ "PRIMARY KEY(c_id, phoneNumber), "
+    					+ "FOREIGN KEY(c_id) REFERENCES Main(c_id) ON DELETE CASCADE "
+    					+ ");";
+
+    /*
+     * address表，存地址(可能多个)
+     * 列依次为：c_id, address
+     */
+    private final static 
+    String ADDRESS_TABLE = "CREATE TABLE IF NOT EXISTS address("
+    					+ "c_id INTEGER NOT NULL, "
+    					+ "address VARCHAR(255), "
+    					+ "PRIMARY KEY(c_id, address), "
+    					+ "FOREIGN KEY(c_id) REFERENCES main(c_id) ON DELETE CASCADE "
+    					+ ");";
+    
+    /*
+     * tag表，存标签，多个
+     * 列依次为：c_id, tag
+     */
+    private final static 
+    String TAG_TABLE = "CREATE TABLE IF NOT EXISTS tag( "
+    					+ "c_id INTEGER NOT NULL, "
+    					+ "tag VARCHAR(50),"
+    					+ "PRIMARY KEY(c_id, tag), " 
+    					+ "FOREIGN KEY(c_id) REFERENCES main(c_id) ON DELETE CASCADE "
+    					+ ");";
     
     public DatabaseHelper(Context context, String name, CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -39,7 +82,17 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
+    	try {
+			// 依次创建四个表
+			db.execSQL(MAIN_TABLE);
+			db.execSQL(ADDRESS_TABLE);
+			db.execSQL(PHONE_NUMBER_TABLE);
+			db.execSQL(TAG_TABLE);
+		} catch (SQLException se) {
+			// TODO Auto-generated catch block
+			se.printStackTrace();
+			Log.i("err", "create table failed.");
+		}
     }
 
     @Override
